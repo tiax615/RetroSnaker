96a071feaee29b6d274dd897c7b41acfa9f3fd3f
cd <cd@ahucd.cn>
2021-09-22 15:55:42 +0800

    !feat: 20210922 Finish

---
+++ Source/RetroSnaker/RetroSnakerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RetroSnakerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RETROSNAKER_API ARetroSnakerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
